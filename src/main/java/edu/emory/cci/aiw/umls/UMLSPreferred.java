5c474691eebfceedcf4898882e5c8eed288cf624
Michel Mansour <michel.mansour@emory.edu>
2010-08-16 13:50:08 +0000

    changed to enum

--- src/main/java/edu/emory/cci/aiw/umls/UMLSPreferred.java
+++ src/main/java/edu/emory/cci/aiw/umls/UMLSPreferred.java
@@ -1,24 +1,5 @@
 package edu.emory.cci.aiw.umls;
 
-public final class UMLSPreferred {
-	
-	private boolean enabled = false;
-	private boolean preference = false;
-	
-	public static final UMLSPreferred NO_PREFERENCE = new UMLSPreferred(false, false);
-	public static final UMLSPreferred NOT_PREFERRED = new UMLSPreferred(true, false);
-	public static final UMLSPreferred PREFERRED     = new UMLSPreferred(true, true);
-	
-	private UMLSPreferred(boolean enabled, boolean preference) {
-		this.enabled = enabled;
-		this.preference = preference;
-	}
-	
-	public boolean equals(Object obj) {
-		if (obj instanceof UMLSPreferred) {
-			return this.enabled == ((UMLSPreferred) obj).enabled && 
-					this.preference == ((UMLSPreferred) obj).preference;
-		}
-		return false;
-	}
+public enum UMLSPreferred {
+    NO_PREFERENCE, NOT_PREFERRED, PREFERRED;
 }
